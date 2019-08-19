c9db87726512ecf3b695b18e4f9951deca056223
Veluxer <48890225+Veluxer@users.noreply.github.com>
2019-08-19 13:18:32 +0200

    nice website
    nice website

---
+++ java_script.js
@@ -0,0 +1,22 @@
+function message()
+{
+var x = prompt("Introduiser votre Nom");
+
+console.log("got :: " + x);
+
+window.alert("bonjour " + x + " c est marie du 72");
+
+}
+
+function call()
+{
+alert("ce site est interdit au mineurs");
+alert("sur ce site tu risque de voir des photos de femmes de ton cartier nues nous demandons donc aux visiteurs de faire preuve de discretion");
+}
+function middlefinger()
+{
+    while 1<100: 
+    {
+    alert("voulez vous vraiment quitter ce site");
+    } 	
+}
